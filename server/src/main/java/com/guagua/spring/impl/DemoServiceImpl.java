a4f6c6263d839af0f736849562e3631b22295177
smile watermelon <18516833610@163.com>
2023-03-15 22:06:29 +0800

    Springboot 整合dubbo

--- server/src/main/java/com/guagua/spring/impl/DemoServiceImpl.java
+++ server/src/main/java/com/guagua/spring/impl/DemoServiceImpl.java
@@ -10,6 +10,7 @@ import org.apache.dubbo.config.annotation.DubboService;
  */
 @DubboService
 public class DemoServiceImpl implements DemoService {
+
     @Override
     public String sayHello(String name) {
         return "hello dubbo " + name ;
